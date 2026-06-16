beb54caf3d5a9a9f04c951f4e6202a29fcb170e3
Facephi <developer@facephi.com>
2026-06-16 15:04:31 +0000

    Promote FPHIDisclaimerWidget SPM 0.1.0

--- Package.swift
+++ Package.swift
@@ -1,27 +1,44 @@
-// swift-tools-version: 6.3
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "FPHIDisclaimerWidget-SPM",
+    defaultLocalization: "es",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FPHIDisclaimerWidget-SPM",
-            targets: ["FPHIDisclaimerWidget-SPM"]
+            targets: ["FPHIDisclaimerWidget-SPM", "FPHIDisclaimerWidget"],
         ),
+    ],
+    dependencies: [
+        .package(url: "git@github.com:facephi-clienters/SDK-FPHIDesignSystemResources-SPM.git", exact: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .plugin(
+            name: "CopyResources",
+            capability: .command(
+                intent: .custom(verb: "copy-local-kmp-resources",
+                                description: "Copy local KMP Resources for SPM"),
+                permissions: [
+                    .writeToPackageDirectory(reason: "Adds generated resources.")
+                ]
+            )
+        ),
         .target(
-            name: "FPHIDisclaimerWidget-SPM"
+            name: "FPHIDisclaimerWidget-SPM",
+            dependencies: [
+                "FPHIDisclaimerWidget",
+                .product(name: "FPHIDesignSystemResources", package: "SDK-FPHIDesignSystemResources-SPM"),
+            ],
+            resources: [.copy("compose/cocoapods/compose-resources")]
         ),
-        .testTarget(
-            name: "FPHIDisclaimerWidget-SPMTests",
-            dependencies: ["FPHIDisclaimerWidget-SPM"]
+        .binaryTarget(
+            name: "FPHIDisclaimerWidget",
+            url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/FPHIDisclaimerWidget/0.1.0/FPHIDisclaimerWidget.zip",
+            checksum: "fc5646944ec07fe824a260c8d12785e1a2664d7b459448d7917174a4d0407fd3"
         ),
-    ],
-    swiftLanguageModes: [.v6]
+    ]
 )
